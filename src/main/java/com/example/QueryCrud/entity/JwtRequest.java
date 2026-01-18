6930871912dfe7e05e0a71a72030f81a2d1dffc6
DhangarNikita <dhangarnikita13@gmail.com>
2026-01-18 17:38:15 +0530

    security

---
+++ src/main/java/com/example/QueryCrud/entity/JwtRequest.java
@@ -0,0 +1,15 @@
+package com.example.QueryCrud.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class JwtRequest {
+
+    private String email;
+    private String password;
+}
